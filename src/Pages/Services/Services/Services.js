bee0589ebac06364f29687646b6d6b549d150a27
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2022-11-09 19:25:51 +0600

    package card in home

--- src/Pages/Services/Services/Services.js
+++ src/Pages/Services/Services/Services.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ServicesCardHome from "../ServicesCardHome/ServicesCardHome";
 
 const Services = () => {
+  const [packages, setPackages] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => setPackages(data));
+  }, []);
+
   return (
     <div className="my-5">
       <h1 className="text-white text-5xl text-center font-serif font-semibold ">
@@ -10,6 +19,14 @@ const Services = () => {
         I'm providing the best Budget Photography & Cinematography service in
         the town.
       </p>
+      <div className="grid grid-cols-3 gap-3 mx-40 my-5">
+        {packages.map((pack) => (
+          <ServicesCardHome key={pack._id} pack={pack}></ServicesCardHome>
+        ))}
+      </div>
+      <div className="text-center">
+        <button className="btn btn-error">See More</button>
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Services/ServicesCardHome/ServicesCardHome.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ServicesCardHome = ({ pack }) => {
+  const { name, picture, _id, price, details } = pack;
+  //   console.log(name);
+  return (
+    <div className="">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={picture} alt="Shoes" className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{name}</h2>
+          <p>
+            Package Details:{" "}
+            {details ? details.slice(0, 100) + "...." : details}
+          </p>
+          <h2>Charges: {price} BDT</h2>
+          <div className="card-actions">
+            <button className="btn btn-secondary">Book A Date</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesCardHome;
